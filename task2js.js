152f5516447075f8c5ae26855e7c53c0eaa1c7c8
bilalsharif30 <bilalsharif418@gmail.com>
2024-11-08 08:12:20 +0100

    Update task2js.js

--- task2js.js
+++ task2js.js
@@ -1,10 +1,10 @@
 document.addEventListener('scroll', function() {
   const scrollPosition = window.scrollY;
-
+  
   const section1 = document.querySelector('#section1');
   const section2 = document.querySelector('#section2');
   const section3 = document.querySelector('#section3');
-  
+
   section1.style.backgroundPositionY = `${scrollPosition * 0.3}px`;
   section2.style.backgroundPositionY = `${(scrollPosition - section2.offsetTop) * 0.3}px`;
   section3.style.backgroundPositionY = `${(scrollPosition - section3.offsetTop) * 0.3}px`;
